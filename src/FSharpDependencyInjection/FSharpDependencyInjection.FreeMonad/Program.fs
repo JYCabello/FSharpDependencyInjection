bfb283266f8a4138b401c5d1aba4755cb619b7e0
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-23 20:58:34 +0200

    free monad-ish

--- src/FSharpDependencyInjection/FSharpDependencyInjection.FreeMonad/Program.fs
+++ src/FSharpDependencyInjection/FSharpDependencyInjection.FreeMonad/Program.fs
@@ -20,6 +20,48 @@ module ImpureInstructions =
     function
     | Free x -> x |> mapI (bind f) |> Free
     | Pure x -> f x
+    
+  type UserBuilder () =
+    member this.Bind (x, f) = bind f x
+    member this.Return x = Pure x
+    member this.ReturnFrom x = x
+    member this.Zero () = Pure ()
+    
+  let user = UserBuilder()
+  
+module UserInstructionsElevations =
+  open ImpureInstructions
+  let getUser id = Free (GetUser (id, Pure))
+  let getSettings userId = Free (GetSettings (userId, Pure))
+  let getDevice userId = Free (GetDevice (userId, Pure))
+
+module Interpreters =
+  open ImpureInstructions
+  let findUser id = { ID = id; Name = "Name"; Email = "email@email.com" }
+  let findSettings userId = { UserID = userId; AreNotificationsEnabled = true }
+  let findDevice userId = { UserID = userId; ID = userId + 7 }
+  
+  let rec interpreter =
+    function
+    | Pure x -> x
+    | Free (GetUser (x, next)) -> x |> findUser |> next |> interpreter
+    | Free (GetSettings (x, next)) -> x |> findSettings |> next |> interpreter
+    | Free (GetDevice (x, next)) -> x |> findDevice |> next |> interpreter
+    
+    
+  
+open ImpureInstructions
+open UserInstructionsElevations
+let x =
+  user {
+    let! user = getUser 5
+    let! settings = getSettings user.ID
+    let! device = getDevice user.ID
+    return device
+  }
+
+let result = Interpreters.interpreter x
+
   
 // For more information see https://aka.ms/fsharp-
-printfn "Hello from F#"
+printfn $"%A{result}"
