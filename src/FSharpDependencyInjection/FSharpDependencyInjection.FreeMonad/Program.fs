b444ecd4e4d13593b96c5342841630b8bdb5459b
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-24 12:38:41 +0200

    Effects, just

--- src/FSharpDependencyInjection/FSharpDependencyInjection.FreeMonad/Program.fs
+++ src/FSharpDependencyInjection/FSharpDependencyInjection.FreeMonad/Program.fs
@@ -29,6 +29,23 @@ module ImpureInstructions =
     
   let user = UserBuilder()
   
+module Effects =
+  open FsToolkit.ErrorHandling
+
+  type Error = string
+  type Effect<'a> = Async<Result<'a, Error>>
+
+  let ofResult r: Effect<'a> = r |> Async.singleton
+  let singleton x: Effect<'a> = x |> Ok |> ofResult
+
+  let bind (f: 'a -> Effect<'b>) (x: Effect<'a>): Effect<'b> = 
+    asyncResult {
+      let! value = x
+      return! f value
+    }
+
+  let (>>=) x f = bind f x 
+  
 module UserInstructionsElevations =
   open ImpureInstructions
   let getUser id = Free (GetUser (id, Pure))
@@ -37,21 +54,29 @@ module UserInstructionsElevations =
 
 module Interpreters =
   open ImpureInstructions
+  open Effects
   let findUser id = { ID = id; Name = "Name"; Email = "email@email.com" }
   let findSettings userId = { UserID = userId; AreNotificationsEnabled = true }
   let findDevice userId = { UserID = userId; ID = userId + 7 }
   
   let rec interpreter =
     function
-    | Pure x -> x
+    | Pure x -> singleton x
     | Free (GetUser (x, next)) -> x |> findUser |> next |> interpreter
     | Free (GetSettings (x, next)) -> x |> findSettings |> next |> interpreter
     | Free (GetDevice (x, next)) -> x |> findDevice |> next |> interpreter
-    
-    
+
+
   
 open ImpureInstructions
 open UserInstructionsElevations
+open FsToolkit.ErrorHandling
+
+type FinalResult =
+  { DeviceID: int
+    ShouldSendEmail: bool
+    Email: string }
+
 let x =
   user {
     let! user = getUser 5
@@ -59,12 +84,19 @@ let x =
     let! device = getDevice user.ID
 
     return
-      {| DeviceID = device.ID
-         ShouldSendEmail = settings.AreNotificationsEnabled
-         Email = user.Email |}
+      { DeviceID = device.ID
+        ShouldSendEmail = settings.AreNotificationsEnabled
+        Email = user.Email }
   }
 
-let result = Interpreters.interpreter x
+let result =
+  Interpreters.interpreter x
+  |> Async.RunSynchronously
+  |> function
+     | Ok value -> $"%A{value}"
+     | Error errorValue -> errorValue
+
+
 
   
 // For more information see https://aka.ms/fsharp-
