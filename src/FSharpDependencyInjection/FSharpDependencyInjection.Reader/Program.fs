ad00ab523b033d4189ac1a4844d44875549e4734
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-26 19:24:39 +0200

    defined ports

--- src/FSharpDependencyInjection/FSharpDependencyInjection.Reader/Program.fs
+++ src/FSharpDependencyInjection/FSharpDependencyInjection.Reader/Program.fs
@@ -5,9 +5,14 @@ open FsToolkit.ErrorHandling
 
 type IPorts =
   abstract member runQuery<'a> : string -> 'a -> Async<Result<'a, DomainError>>
-  abstract member sendEmail : EmailEnvelope -> Async<Result<Unit, DomainError>>
+  abstract member sendEmail: EmailEnvelope -> Async<Result<Unit, DomainError>>
 
-
+type Ports =
+  interface IPorts with
+    member this.runQuery _ defaultValue = defaultValue |> AsyncResult.ok
+    member this.sendEmail _ = () |> AsyncResult.ok
+  
+    
 
 let program () : int -> Async<Result<Unit, DomainError>> =
   fun _ -> "not implmemented" |> InternalServerError |> AsyncResult.error
